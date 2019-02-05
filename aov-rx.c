a0598f9e40e9c965ca4c255e1ff64c81915d691e
Angel Ortega <angel@triptico.com>
2019-02-05 11:54:37 +0100

    Changed licence to public domain.

--- aov-rx.c
+++ aov-rx.c
@@ -2,29 +2,15 @@
 
     aov-rx - Angel Ortega's regular expression library
 
-    Copyright (C) 2011/2012 Angel Ortega <angel@triptico.com>
+    Angel Ortega <angel@triptico.com>
 
-    This program is free software; you can redistribute it and/or
-    modify it under the terms of the GNU General Public License
-    as published by the Free Software Foundation; either version 2
-    of the License, or (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-
-    http://triptico.com
+    Public domain.
 
 */
 
 #include "aov-rx.h"
 
-#define VERSION "0.5.1"
+#define VERSION "0.52"
 
 
 static wchar_t *in_set(wchar_t *rx, wchar_t c, int *found)
